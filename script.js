342bf6b0d4d126efa6b71731d980e4cd9fd38e9c
Olha Shekhet <oliayesss@gmail.com>
2023-02-23 15:01:40 -0800

    setupping

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const appEl = document.querySelector('.app')
 const resultTextEl = document.querySelector('.result-text')
 const restartBtn = document.querySelector('.restart-btn')
 
+const cellList = document.querySelectorAll('.cell')
+
+let game = [[null,null,null], [null,null,null], [null,null,null]]
+
